89003027baeea802f457e1608a0ca4c511827608
vartdalen <viktor.v.johansen@hotmail.com>
2021-01-03 18:24:23 +0100

    Migrate from H2 to SQL database Create working SQL database configuration Implement ImageOrientation enum Implement ImageCategory enum

--- image-store-sql/src/main/java/com/vartdalen/imagestoresql/model/Image.java
+++ image-store-sql/src/main/java/com/vartdalen/imagestoresql/model/Image.java
@@ -1,42 +1,57 @@
 package com.vartdalen.imagestoresql.model;
+import com.vartdalen.imagestoresql.model.enumerator.ImageCategory;
+import com.vartdalen.imagestoresql.model.enumerator.ImageOrientation;
+
 import javax.persistence.*;
 import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
-@Table
+@Table(name = "image")
 public class Image {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column
+    @Column(name = "id")
     private long id;
-    @Column
+    @Column(name = "title")
     private String title;
-    @Column
-    private String category;
-    @Column
+    @Column(name = "orientation")
+    @Enumerated(EnumType.STRING)
+    private ImageOrientation orientation;
+    @ElementCollection(targetClass = ImageCategory.class)
+    @CollectionTable(name = "image_category", joinColumns = @JoinColumn(name = "id"))
+    @Enumerated(EnumType.STRING)
+    @Column(name = "category")
+    private Set<ImageCategory> categories;
+    @Column(name = "bytes")
     private byte[] bytes;
-    @Column
+    @Column(name = "created")
     private Timestamp created;
 
     public Image() {}
 
-    public Image(long id, String title, String category, byte[] bytes, Timestamp created) {
+    public Image(long id, String title, ImageOrientation orientation, ImageCategory[] categories, byte[] bytes, Timestamp created) {
         this.id = id;
         this.title = title;
-        this.category = category;
+        this.orientation = orientation;
+        this.categories = new HashSet<>(Arrays.asList(categories));
         this.bytes = bytes;
         this.created = created;
     }
 
     public long getId() { return id; }
     public String getTitle() { return title; }
-    public String getCategory() { return category; }
+    public ImageOrientation getOrientation() { return orientation; }
+    public Set<ImageCategory> getCategories() { return categories; }
     public byte[] getBytes() { return bytes; }
     public Timestamp getCreated() { return created; }
 
     public void setId(long id) { this.id = id; }
     public void setTitle(String title) { this.title = title; }
-    public void setCategory(String category) { this.category = category; }
+    public void setOrientation(ImageOrientation orientation) { this.orientation = orientation; }
+    public void setCategories(ImageCategory[] categories) { this.categories = new HashSet<>(Arrays.asList(categories)); }
     public void setBytes(byte[] bytes) { this.bytes = bytes; }
     public void setCreated(Timestamp created) { this.created = created; }
 }
