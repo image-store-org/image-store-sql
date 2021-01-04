3a3a4ef16ee3a3012e95f9fd25caedbae9b05d12
vartdalen <viktor.v.johansen@hotmail.com>
2021-01-04 20:23:45 +0100

    Enhance, fix and test REST and CRUD functionality Enhance database

--- image-store-sql/src/main/java/com/vartdalen/imagestoresql/model/Image.java
+++ image-store-sql/src/main/java/com/vartdalen/imagestoresql/model/Image.java
@@ -1,9 +1,12 @@
 package com.vartdalen.imagestoresql.model;
 import com.vartdalen.imagestoresql.model.enumerator.ImageCategory;
 import com.vartdalen.imagestoresql.model.enumerator.ImageOrientation;
+import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
@@ -11,34 +14,45 @@ import java.util.Set;
 @Entity
 @Table(name = "image")
 public class Image {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
     @Column(name = "id")
     private long id;
+
+    @NotNull
     @Column(name = "title")
     private String title;
-    @Column(name = "orientation")
+
     @Enumerated(EnumType.STRING)
+    @NotNull
+    @Column(name = "orientation")
     private ImageOrientation orientation;
+
     @ElementCollection(targetClass = ImageCategory.class)
     @CollectionTable(name = "image_category", joinColumns = @JoinColumn(name = "id"))
     @Enumerated(EnumType.STRING)
+    @NotNull
     @Column(name = "category")
     private Set<ImageCategory> categories;
+
+    @NotNull
     @Column(name = "bytes")
     private byte[] bytes;
-    @Column(name = "created")
+
+    @CreationTimestamp
+    @Column(name = "created", nullable = false, insertable = false, updatable = false, columnDefinition="TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private Timestamp created;
 
     public Image() {}
 
-    public Image(long id, String title, ImageOrientation orientation, ImageCategory[] categories, byte[] bytes, Timestamp created) {
+    public Image(long id, String title, ImageOrientation orientation, ImageCategory[] categories, byte[] bytes) {
         this.id = id;
         this.title = title;
         this.orientation = orientation;
         this.categories = new HashSet<>(Arrays.asList(categories));
         this.bytes = bytes;
-        this.created = created;
     }
 
     public long getId() { return id; }
@@ -46,12 +60,11 @@ public class Image {
     public ImageOrientation getOrientation() { return orientation; }
     public Set<ImageCategory> getCategories() { return categories; }
     public byte[] getBytes() { return bytes; }
-    public Timestamp getCreated() { return created; }
+    public LocalDateTime getCreated() { return created.toLocalDateTime(); }
 
     public void setId(long id) { this.id = id; }
     public void setTitle(String title) { this.title = title; }
     public void setOrientation(ImageOrientation orientation) { this.orientation = orientation; }
     public void setCategories(ImageCategory[] categories) { this.categories = new HashSet<>(Arrays.asList(categories)); }
     public void setBytes(byte[] bytes) { this.bytes = bytes; }
-    public void setCreated(Timestamp created) { this.created = created; }
 }
