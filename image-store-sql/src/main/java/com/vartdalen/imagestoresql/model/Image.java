e039816d4d8d08df4670d71b17ba6b666f765540
vartdalen <viktor.v.johansen@hotmail.com>
2021-04-05 01:53:24 +0200

    Create ImageExtension field

--- image-store-sql/src/main/java/com/vartdalen/imagestoresql/model/Image.java
+++ image-store-sql/src/main/java/com/vartdalen/imagestoresql/model/Image.java
@@ -1,6 +1,8 @@
 package com.vartdalen.imagestoresql.model;
 import com.vartdalen.imagestoresql.model.enumerator.ImageCategory;
+import com.vartdalen.imagestoresql.model.enumerator.ImageExtension;
 import com.vartdalen.imagestoresql.model.enumerator.ImageOrientation;
+
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
@@ -29,17 +31,18 @@ public class Image {
     @NotNull
     @Column(name = "orientation")
     private ImageOrientation orientation;
-
+    
     @ElementCollection(targetClass = ImageCategory.class)
     @CollectionTable(name = "image_category", joinColumns = @JoinColumn(name = "id"))
     @Enumerated(EnumType.STRING)
     @NotNull
     @Column(name = "category")
     private Set<ImageCategory> categories;
-
+    
+    @Enumerated(EnumType.STRING)
     @NotNull
-    @Column(name = "bytes")
-    private byte[] bytes;
+    @Column(name = "extension")
+    private ImageExtension extension;
 
     @CreationTimestamp
     @Column(name = "created", nullable = false, insertable = false, updatable = false, columnDefinition="TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
@@ -47,24 +50,24 @@ public class Image {
 
     public Image() {}
 
-    public Image(long id, String title, ImageOrientation orientation, ImageCategory[] categories, byte[] bytes) {
+    public Image(long id, String title, ImageOrientation orientation, ImageCategory[] categories, ImageExtension extension) {
         this.id = id;
         this.title = title;
         this.orientation = orientation;
         this.categories = new HashSet<>(Arrays.asList(categories));
-        this.bytes = bytes;
+        this.extension = extension;
     }
 
     public long getId() { return id; }
     public String getTitle() { return title; }
     public ImageOrientation getOrientation() { return orientation; }
     public Set<ImageCategory> getCategories() { return categories; }
-    public byte[] getBytes() { return bytes; }
+    public ImageExtension getExtension() { return extension; }
     public LocalDateTime getCreated() { return created.toLocalDateTime(); }
 
     public void setId(long id) { this.id = id; }
     public void setTitle(String title) { this.title = title; }
     public void setOrientation(ImageOrientation orientation) { this.orientation = orientation; }
     public void setCategories(ImageCategory[] categories) { this.categories = new HashSet<>(Arrays.asList(categories)); }
-    public void setBytes(byte[] bytes) { this.bytes = bytes; }
+    public void setExtension(ImageExtension extension) { this.extension = extension; }
 }
